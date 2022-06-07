536008f3b13950f06c88e2be663fc7cf9ed86464
Rohit Maity <official.rohitmaity@icloud.com>
2022-06-07 21:40:27 +0530

    Commit Made on 7th June 2022

---
+++ Python_Projects/SQL_DataQuery.py
@@ -0,0 +1,38 @@
+import pyodbc
+import datetime
+
+server = 'localhost\SQLEXPRESS'
+database = 'IOT_Database'
+username = 'sa'
+password = 'Admin@1234'
+connection = pyodbc.connect('DRIVER={ODBC Driver 11 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+if(connection):
+    print("Connection Successful");
+else:
+    print("Connection Failed");
+
+cursor=connection.cursor()
+
+cursor.execute(
+    """
+    SELECT * FROM IOT_Database.dbo.Sensor_Data
+    
+    """
+)
+
+row = cursor.fetchone() 
+
+print(row)
+
+while(row):
+    print(row[0])
+    print(row[1])
+    print(row[2])
+    print(row[3])
+
+    row=cursor.fetchone()
+
+connection.close()
+
+
